8ff3df50f0e877e46df858c4a9d9af8cc7842014
Vikas <70474910+Vikas1109@users.noreply.github.com>
2020-09-17 20:56:37 +0530

    initial commit.

---
+++ module_4-solution.js
@@ -0,0 +1,34 @@
+
+
+// function user_input (name) {
+
+//     if (name[0] === "j" || name[0] === "J")
+//     {
+//         console.log("Good Bye " + name);
+//     } 
+
+//     else
+//     {
+//         console.log("Hello " + name);
+//     }
+
+
+// }
+
+// user_input("Govind");
+
+// user_input("Jockey");
+
+// user_input("Yaakov")
+
+
+var list = ["hello" , "world" , "vikas chauhan" , "shubham Rawat" , "suresh" ,"deepti mam"];
+
+for (i in list) {
+
+    console.log(list[i]);
+
+}
+
+
+console.log(length(list))
